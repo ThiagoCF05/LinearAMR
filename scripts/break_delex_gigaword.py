1960fb3543ff7bae6dd9831c97af6abe6a67818d
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-05-04 09:13:52 -0300

    Merge gigaword corpus

--- scripts/break_delex_gigaword.py
+++ scripts/break_delex_gigaword.py
@@ -4,14 +4,13 @@ __author__ = 'thiagocastroferreira'
 Author: Thiago Castro Ferreira
 Date: 02/05/2017
 Description:
-    This script aims to break the Gigaword corpus in subfiles with 2 million sentences to boost the parsing performed
+    This script aims to break (and unite) the Gigaword corpus in subfiles with 2 million sentences to boost the parsing performed
     by delex_gigaword.py
 """
 
+import os
 
-if __name__ == '__main__':
-    gigaword = '/roaming/tcastrof/gigaword/delex/gigaword.delex'
-
+def _break(gigaword):
     f = open(gigaword)
     doc = f.read()
     f.close()
@@ -28,3 +27,22 @@ if __name__ == '__main__':
 
             f = open(gigaword+str(nfiles), 'w')
     f.close()
+
+def _unite(split_gigaword, wgigaword):
+    f = open(wgigaword, 'w')
+    for fname in os.listdir(split_gigaword):
+        if 'gigaword.tok.delex' in fname:
+            g = open(os.path.join(split_gigaword, fname), 'w')
+            doc = g.read()
+            g.close()
+
+            f.write(doc)
+    f.close()
+
+if __name__ == '__main__':
+    # gigaword = '/roaming/tcastrof/gigaword/delex/gigaword.delex'
+    # _break(gigaword)
+
+    split_gigaword = '/roaming/tcastrof/gigaword/delex'
+    wgigaword = '/roaming/tcastrof/gigaword/gigaword.tok.delex'
+    _unite(split_gigaword, wgigaword)
